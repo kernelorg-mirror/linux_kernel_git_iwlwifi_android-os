dcbcca5968f4b2faf8625deb1e9851c03ecd8a4d
Ilan Peer <ilan.peer@intel.com>
2025-06-27 11:22:30 +0300

    ANDROID: iwl7000: mac80211: Accept management frames on NAN interface
    Accept Public Action frames and Authentication frames on
    NAN Device interface to support flows that require these frames:
    
    - SDFs: For user space Discovery Engine (DE) implementation.
    - NAFs: For user space NAN Data Path (NDP) establishment.
    - Authentication frames: For NAN Pairing and Verification.
    
    Accept only frames from devices that are part of the NAN
    cluster.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Change-Id: Ifccd87fb2a49a3af05238f74f52fa6da8de28811
    iwl7000-tree: eda220a777142fb2a03167625fa718433962bdd3

--- mac80211/rx.c
+++ mac80211/rx.c
@@ -4485,7 +4485,11 @@ static bool ieee80211_accept_frame(struct ieee80211_rx_data *rx)
 		       (ieee80211_is_auth(hdr->frame_control) &&
 			ether_addr_equal(sdata->vif.addr, hdr->addr1));
 	case NL80211_IFTYPE_NAN:
-		/* Currently no frames on NAN interface are allowed */
+		/* Accept only frames that are addressed to the NAN cluster
+		 * (based on the Cluster ID). From these frames, accept only
+		 * action frames or authentication frames that are addressed to
+		 * the local NAN interface.
+		 */
 		return false;
 	default:
 		break;
