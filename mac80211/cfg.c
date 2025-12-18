f5fe662664ebf7ac893f9c7d96a7908785444a3a
Avraham Stern <avraham.stern@intel.com>
2025-12-18 04:31:13 +0200

    ANDROID: iwl7000: cfg80211: support key installation on non-netdev wdevs
    Currently key installation is only supported for netdev. For NAN,
    support most key operations (except setting default data key) on
    wdevs instead of netdevs, and adjust all the APIs and tracing to
    match.
    
    Since nothing currently sets NL80211_EXT_FEATURE_SECURE_NAN, this
    doesn't change anything (P2P Device already isn't allowed.)
    
    Change-Id: I00efdf3b2c11efab82ef6ece9f393382bcf33ba8
    Signed-off-by: Avraham Stern <avraham.stern@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: 76700473596836e2b2c56995270ba871b85c20de

--- mac80211/cfg.c
+++ mac80211/cfg.c
@@ -5,7 +5,7 @@
  * Copyright 2006-2010	Johannes Berg <johannes@sipsolutions.net>
  * Copyright 2013-2015  Intel Mobile Communications GmbH
  * Copyright (C) 2015-2017 Intel Deutschland GmbH
- * Copyright (C) 2018-2025 Intel Corporation
+ * Copyright (C) 2018-2026 Intel Corporation
  */
 
 #include <linux/ieee80211.h>
@@ -570,7 +570,8 @@ static int ieee80211_add_key(struct wiphy *wiphy, struct net_device *dev,
 			     int link_id, u8 key_idx, bool pairwise,
 			     const u8 *mac_addr, struct key_params *params)
 {
-	struct ieee80211_sub_if_data *sdata = IEEE80211_DEV_TO_SUB_IF(dev);
+	struct wireless_dev *wdev = dev->ieee80211_ptr;
+	struct ieee80211_sub_if_data *sdata = IEEE80211_WDEV_TO_SUB_IF(wdev);
 	struct ieee80211_link_data *link =
 		ieee80211_link_or_deflink(sdata, link_id, false);
 	struct ieee80211_local *local = sdata->local;
@@ -745,7 +746,8 @@ static int ieee80211_del_key(struct wiphy *wiphy, struct net_device *dev,
 			     int link_id, u8 key_idx, bool pairwise,
 			     const u8 *mac_addr)
 {
-	struct ieee80211_sub_if_data *sdata = IEEE80211_DEV_TO_SUB_IF(dev);
+	struct wireless_dev *wdev = dev->ieee80211_ptr;
+	struct ieee80211_sub_if_data *sdata = IEEE80211_WDEV_TO_SUB_IF(wdev);
 	struct ieee80211_local *local = sdata->local;
 	struct ieee80211_key *key;
 
@@ -766,6 +768,7 @@ static int ieee80211_get_key(struct wiphy *wiphy, struct net_device *dev,
 			     void (*callback)(void *cookie,
 					      struct key_params *params))
 {
+	struct wireless_dev *wdev = dev->ieee80211_ptr;
 	struct ieee80211_sub_if_data *sdata;
 	u8 seq[6] = {0};
 	struct key_params params;
@@ -776,7 +779,7 @@ static int ieee80211_get_key(struct wiphy *wiphy, struct net_device *dev,
 	int err = -ENOENT;
 	struct ieee80211_key_seq kseq = {};
 
-	sdata = IEEE80211_DEV_TO_SUB_IF(dev);
+	sdata = IEEE80211_WDEV_TO_SUB_IF(wdev);
 
 	rcu_read_lock();
 
@@ -883,7 +886,8 @@ static int ieee80211_config_default_mgmt_key(struct wiphy *wiphy,
 					     struct net_device *dev,
 					     int link_id, u8 key_idx)
 {
-	struct ieee80211_sub_if_data *sdata = IEEE80211_DEV_TO_SUB_IF(dev);
+	struct wireless_dev *wdev = dev->ieee80211_ptr;
+	struct ieee80211_sub_if_data *sdata = IEEE80211_WDEV_TO_SUB_IF(wdev);
 	struct ieee80211_link_data *link =
 		ieee80211_link_or_deflink(sdata, link_id, true);
 
@@ -899,7 +903,8 @@ static int ieee80211_config_default_beacon_key(struct wiphy *wiphy,
 					       struct net_device *dev,
 					       int link_id, u8 key_idx)
 {
-	struct ieee80211_sub_if_data *sdata = IEEE80211_DEV_TO_SUB_IF(dev);
+	struct wireless_dev *wdev = dev->ieee80211_ptr;
+	struct ieee80211_sub_if_data *sdata = IEEE80211_WDEV_TO_SUB_IF(wdev);
 	struct ieee80211_link_data *link =
 		ieee80211_link_or_deflink(sdata, link_id, true);
 
