fcdd43967d42b83734a6db695daf6d81de73c0d1
Ilan Peer <ilan.peer@intel.com>
2026-01-06 05:38:07 +0200

    ANDROID: iwl7000: mac80211: allow userspace TX/RX over NAN Data interfaces
    Allow TX/RX of action frames (for NAN action frames) over
    NAN Data interfaces to support cases where there's a secure
    NDP and NAFs may be exchanged over that.
    
    Change-Id: Ic4b1c90552497b7ff2616651a408e5fe8bde7b99
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: bc7bc6881a6ffbb992a4b3c9b9f43bf58c8430ae

--- mac80211/main.c
+++ mac80211/main.c
@@ -5,7 +5,7 @@
  * Copyright 2006-2007	Jiri Benc <jbenc@suse.cz>
  * Copyright 2013-2014  Intel Mobile Communications GmbH
  * Copyright (C) 2017     Intel Deutschland GmbH
- * Copyright (C) 2018-2025 Intel Corporation
+ * Copyright (C) 2018-2026 Intel Corporation
  */
 
 #include <net/mac80211.h>
