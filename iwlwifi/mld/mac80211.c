891168bf44dde327d400f8af8c50731630f080e9
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2026-01-11 14:22:10 +0200

    ANDROID: iwl7000: mld: fix setting phy id in TLC cmd
    The commit mentioned below wanted to remove the chan_ctx argument from
    iwl_mld_tlc_update_phy. I assumed that it is fine since the TLC code
    (iwl_mld_config_tlc_link) knows to dereference the chanctx pointer from the
    link on its own.
    While this assumption holds in most cases, it doesn't for the (un)assign
    link flow. In this flow, mac80211 updates the chan_ctx pointer only after
    the driver returns.
    So we were dereferencing the pointer to the old chanctx.
    
    Fix this by retrieving the pointer from the mld_link instead.
    Also, remove the chan_ctx argument from iwl_mld_tlc_update_phy.
    This required to call iwl_mld_tlc_update_phy a bit later in the unassign
    flow, to after we NULLified the chanctx pointer from the mld_link.
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: I7664b2f74d06ccbfe0e5f33acc46952c251c5c26
    iwl7000-tree: 5bd04644b61f18b602816300b18be8d4e074350b

--- iwlwifi/mld/mac80211.c
+++ iwlwifi/mld/mac80211.c
@@ -1274,7 +1274,7 @@ int iwl_mld_assign_vif_chanctx(struct ieee80211_hw *hw,
 
 	/* Now activate the link */
 	if (iwl_mld_can_activate_link(mld, vif, link)) {
-		iwl_mld_tlc_update_phy(mld, vif, link, ctx);
+		iwl_mld_tlc_update_phy(mld, vif, link);
 
 		ret = iwl_mld_activate_link(mld, link);
 		if (ret)
@@ -1326,8 +1326,6 @@ void iwl_mld_unassign_vif_chanctx(struct ieee80211_hw *hw,
 
 	iwl_mld_deactivate_link(mld, link);
 
-	iwl_mld_tlc_update_phy(mld, vif, link, NULL);
-
 	if (vif->type == NL80211_IFTYPE_MONITOR)
 		iwl_mld_remove_mon_sta(mld, vif, link);
 
@@ -1341,6 +1339,8 @@ void iwl_mld_unassign_vif_chanctx(struct ieee80211_hw *hw,
 	}
 
 	RCU_INIT_POINTER(mld_link->chan_ctx, NULL);
+
+	iwl_mld_tlc_update_phy(mld, vif, link);
 
 	/* in the non-MLO case, remove/re-add the link to clean up FW state.
 	 * In MLO, it'll be done in drv_change_vif_link
