646ec9005a8b6a5b31a5fcaf9d5bd9ec593585b3
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2026-01-20 16:57:36 +0200

    iwl7000: release core102-60
    Change-Id: I5901ee368d11193b99801612f8a8cca2d2bf5ea0
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>

--- hdrs/backports.h
+++ hdrs/backports.h
@@ -4,28 +4,6 @@
  * Copyright (C) 2018-2024 Intel Corporation
  */
 
-/* backport wiphy_ext_feature_set/_isset
- *
- * To do so, define our own versions thereof that check for a negative
- * feature index and in that case ignore it entirely. That allows us to
- * define the ones that the cfg80211 version doesn't support to -1.
- */
-static inline void iwl7000_wiphy_ext_feature_set(struct wiphy *wiphy, int ftidx)
-{
-	if (ftidx < 0)
-		return;
-	wiphy_ext_feature_set(wiphy, ftidx);
-}
-
-static inline bool iwl7000_wiphy_ext_feature_isset(struct wiphy *wiphy,
-						   int ftidx)
-{
-	if (ftidx < 0)
-		return false;
-	return wiphy_ext_feature_isset(wiphy, ftidx);
-}
-#define wiphy_ext_feature_set iwl7000_wiphy_ext_feature_set
-#define wiphy_ext_feature_isset iwl7000_wiphy_ext_feature_isset
 
 static inline enum ieee80211_ap_reg_power
 cfg80211_6ghz_power_type(u8 control, u32 client_flags)
