4400d54f8f2167f74083b7d0aac7a9786ef181c4
Johannes Berg <johannes.berg@intel.com>
2026-01-28 12:24:56 +0100

    ANDROID: iwl7000: align UHR code with upstream
    Sending the patches upstream resulted in some changes,
    align our internal code to them.
    
    Change-Id: I5e5b8f7a057b7a2bb575c9e5bfb25f9e4102fb3b
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: f0dd7b9122010134aada0e29bd6214f22ce108af

--- mac80211/main.c
+++ mac80211/main.c
@@ -1452,8 +1452,8 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 
 	if (supp_uhr)
 		local->scan_ies_len +=
-			3 + sizeof(struct ieee80211_uhr_capa) +
-			sizeof(struct ieee80211_uhr_capa_phy);
+			3 + sizeof(struct ieee80211_uhr_cap) +
+			sizeof(struct ieee80211_uhr_cap_phy);
 
 	if (!local->ops->hw_scan) {
 		/* For hw_scan, driver needs to set these up. */
