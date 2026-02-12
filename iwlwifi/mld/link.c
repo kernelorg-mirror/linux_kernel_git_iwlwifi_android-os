68c58d76ce89248a458968ae9d7c0e6e6068cbf7
Johannes Berg <johannes.berg@intel.com>
2026-02-12 15:45:09 +0200

    ANDROID: iwl7000: mld: implement UHR DPS
    For UHR, implement dynamic power save (DPS). Since the firmware
    handles most of it, simply advertise DPS capability and set the
    corresponding link flag when the AP has advertised that it is a
    DPS assisting AP.
    
    DPS assisting is currently not supported.
    
    Change-Id: I0e0dfcd2e2641a5656a14f01ce696443ae10bc4a
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: 2f86569b72a3242e0978d82b5a3e7ce65d146c82

--- iwlwifi/mld/link.c
+++ iwlwifi/mld/link.c
@@ -363,7 +363,10 @@ iwl_mld_change_link_in_fw(struct iwl_mld *mld, struct ieee80211_bss_conf *link,
 			link_sta_dereference_check(mld_vif->ap_sta,
 						   link->link_id);
 
-		if (!WARN_ON(!link_sta) && link_sta->he_cap.has_he &&
+		if (WARN_ON(!link_sta))
+			return -EINVAL;
+
+		if (link_sta->he_cap.has_he &&
 		    link_sta->he_cap.he_cap_elem.mac_cap_info[5] &
 		    IEEE80211_HE_MAC_CAP5_OM_CTRL_UL_MU_DATA_DIS_RX)
 			cmd.ul_mu_data_disable = 1;
