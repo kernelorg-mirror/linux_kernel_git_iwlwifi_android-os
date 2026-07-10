2b55de060e57c66a1d268d521df6e5469950071c
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2026-07-10 19:01:12 +0300

    ANDROID: iwl7000: add a select for UHR backport
    Google's kernel doesn't require to backport UHR declarations while the
    internal manifest does.
    
    Change-Id: I5b4b0c50db1836554a0493be9b56644feb700a6a
    iwl7000-tree: 0bb0437a057374694c7a0b2cfe60e1912842a9e0

--- hdrs/backports.h
+++ hdrs/backports.h
@@ -145,24 +145,6 @@ cfg80211_6ghz_power_type(u8 control, u32 client_flags)
 #define kvzalloc_flex(P, FAM, COUNT, ...) \
 	__alloc_flex(kvzalloc, default_gfp(__VA_ARGS__), typeof(P), FAM, COUNT)
 
-#define IEEE80211_CHAN_NO_UHR 0
-#define NL80211_RRF_NO_UHR 0
-#define ASSOC_REQ_DISABLE_UHR 0
-
-struct ieee80211_sta_uhr_cap {
-	bool has_uhr;
-	struct ieee80211_uhr_cap_mac mac;
-	struct ieee80211_uhr_cap_phy phy;
-};
-
-static inline const struct ieee80211_sta_uhr_cap *
-ieee80211_get_uhr_iftype_cap(const struct ieee80211_supported_band *sband,
-			     enum nl80211_iftype iftype)
-{
-	return NULL;
-}
-
-
 #include <crypto/aes.h>
 #include <linux/string.h>
 #include <crypto/utils.h>
