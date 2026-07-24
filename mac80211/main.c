32257ea9a0ba57a65c289c0321d7452dfc108a14
Felix Fietkau <nbd@nbd.name>
2026-07-24 11:54:28 +0000

    ANDROID: iwl7000: mac80211: add AQL support for multicast packets
    Excessive multicast traffic with little competing unicast traffic can easily
    flood hardware queues, leading to throughput issues. Additionally, filling
    the hardware queues with too many packets breaks FQ for multicast data.
    Fix this by enabling AQL for multicast packets.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Link: https://patch.msgid.link/20260724115429.3921457-3-nbd@nbd.name
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: I6223071d77e02edf1f546857cee8d5f7c9a76219
    iwl7000-tree: 83cd37790767a9ce4d4f2b21c6f1e0a1d8772351

--- mac80211/main.c
+++ mac80211/main.c
@@ -986,6 +986,7 @@ struct ieee80211_hw *ieee80211_alloc_hw_nm(size_t priv_data_len,
 	spin_lock_init(&local->rx_path_lock);
 	spin_lock_init(&local->queue_stop_reason_lock);
 
+	local->aql_txq_limit_mc = 50000;
 	for (i = 0; i < IEEE80211_NUM_ACS; i++) {
 		INIT_LIST_HEAD(&local->active_txqs[i]);
 		spin_lock_init(&local->active_txq_lock[i]);
